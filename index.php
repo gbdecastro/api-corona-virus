f04c9a0c9501101647157d3dc31182f7bcf04fb2
Gabriel de Castro <gabriel_castr@outlook.com.br>
2020-03-21 12:20:56 -0400

    heroky

--- index.php
+++ index.php
@@ -17,13 +17,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $path = $_SERVER['REQUEST_URI'];
 
     if (preg_match("/\b(\/.+)\??(.+)?\b/", $path, $m)) {
-        switch ($path) {
-            case '/api/data/world':
+        $route = $m[1];
+        switch ($route) {
+            case '/data/world':
                 $data = file_get_contents('https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/1/query?f=json&where=Confirmed%20%3E%200&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outSR=102100&cacheHint=true');
                 $data = json_decode($data, true);
                 echo json_encode($data['features']);
                 break;
-            case '/api/data/brazil':
+            case '/data/brazil':
                 $data = str_replace("var dados = ", "", file_get_contents('https://sigageomarketing.com.br/coronavirus/coronavirus.js'));
                 $data = json_decode($data, true);
                 echo json_encode($data['features']);
