25f3cbe0706126b010c9326d4da74d0acc6823c5
Gabriel de Castro <gabriel_castr@outlook.com.br>
2020-03-21 11:40:13 -0400

    Removendo o preg_match

--- index.php
+++ index.php
@@ -2,50 +2,46 @@
 
 header("Content-type:application/json");
 
-include("php-cors/src/Cors.php");
+include "php-cors/src/Cors.php";
 
 new \Lz\PHP\Cors([
-    'origin'      => '*',
+    'origin' => '*',
     'credentials' => true,
-    'max-age'     => 86400,
-    'headers'     => ['Content-Type', 'Accept', 'Origin', 'Authorization'],
-    'methods'     => ['GET']
+    'max-age' => 86400,
+    'headers' => ['Content-Type', 'Accept', 'Origin', 'Authorization'],
+    'methods' => ['GET'],
 ]);
 
-if($_SERVER['REQUEST_METHOD'] == 'GET'){
-    if(preg_match("/\bindex.php\/(.+)\b/", $_SERVER['PHP_SELF'], $matched)){
-        switch ($matched[1]) {
-            case 'api/data/world':
-                $data = file_get_contents('https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/1/query?f=json&where=Confirmed%20%3E%200&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outSR=102100&cacheHint=true');
-                $data = json_decode($data,true);
+echo preg_match("/\b\/(.+)??(.+)?\b/", $_SERVER['REQUEST_URI'], $matched);
 
-                foreach ($data['features'] as $key => $value) {
-                    
-                }
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
-                echo json_encode($data['features']);
-                break;
-            case 'api/data/brazil':
-                $data = str_replace("var dados = ","",file_get_contents('https://sigageomarketing.com.br/coronavirus/coronavirus.js'));
-                $data = json_decode($data,true);
-                echo json_encode($data['features']);                
-                break;
-            default:
-                echo json_encode([
-                    "code" => "404",
-                    "message" => "URL Request not found"
-                ]);
-                break;
-        }  
-    }else{
-        echo json_encode([
-            "code" => "404",
-            "message" => "URL Request  not found"
-        ]);
+    switch ($_SERVER['REQUEST_URI']) {
+        case '/api/data/world':
+            $data = file_get_contents('https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/1/query?f=json&where=Confirmed%20%3E%200&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outSR=102100&cacheHint=true');
+            $data = json_decode($data, true);
+
+            foreach ($data['features'] as $key => $value) {
+
+            }
+
+            echo json_encode($data['features']);
+            break;
+        case '/api/data/brazil':
+            $data = str_replace("var dados = ", "", file_get_contents('https://sigageomarketing.com.br/coronavirus/coronavirus.js'));
+            $data = json_decode($data, true);
+            echo json_encode($data['features']);
+            break;
+        default:
+            echo json_encode([
+                "code" => "404",
+                "message" => "URL Request not found",
+            ]);
+            break;
     }
-}else{
+} else {
     echo json_encode([
         "code" => "404",
-        "message" => "The method ". $_SERVER['REQUEST_METHOD']. "not exists"
-    ]);    
+        "message" => "The method " . $_SERVER['REQUEST_METHOD'] . "not exists",
+    ]);
 }
